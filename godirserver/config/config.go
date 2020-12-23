dab67ad0387805aed110312e44f8ec4f4277c875
william <william@william.com>
2020-12-23 15:30:40 +0800

    auto create dir

--- godirserver/config/config.go
+++ godirserver/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"log"
 	"os"
 	"path"
 )
@@ -13,11 +14,26 @@ func init() {
 	p, _ := os.Getwd()
 	APPPATH = p
 	if len(os.Args) > 1 {
-		BASEPATH = path.Join(APPPATH,os.Args[1])
-	}else{
-		BASEPATH = path.Join(APPPATH,"images")
+		BASEPATH = path.Join(APPPATH, os.Args[1])
+	} else {
+		BASEPATH = path.Join(APPPATH, "images")
+	}
+	s, err := os.Stat(BASEPATH)
+	if err != nil {
+		err = os.Mkdir(BASEPATH, os.ModeDir)
+		if err != nil {
+			log.Fatal("Mkdir BASEPATH fail.")
+		}
+	} else if s != nil && s.IsDir() == false {
+		err = os.Remove(BASEPATH)
+		if err != nil {
+			log.Fatal("Remove BASEPATH fail")
+		}
+		
+		err = os.Mkdir(BASEPATH, os.ModeDir)
+		if err != nil {
+			log.Fatal("Mkdir BASEPATH fail")
+		}
+		
 	}
 }
-
-
-
--- godirserver/controller/index.go
+++ godirserver/controller/index.go
@@ -17,9 +17,14 @@ import (
 	"strings"
 )
 
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
+
 func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
-
+	
 	p := r.FormValue("p")
 	if p == "" {
 		p = "/"
@@ -29,9 +34,9 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	if s.IsDir() {
 		dirs, files := utils.ScanDir(p)
 		resData := model.JsonResult{Code: 200, Msg: "", Data: model.Res{Dirs: dirs, Files: files}}
-
+		
 		msg, _ := json.Marshal(resData)
-
+		
 		//fmt.Fprintf(w, "hello world")
 		w.Header().Set("content-type", "text/json")
 		w.Write(msg)
@@ -40,19 +45,21 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		fData, _ := ioutil.ReadFile(path.Join(config.BASEPATH, p))
 		w.Write(fData)
 	}
-
+	
 }
 func Upload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
-	w.Header().Set("Access-Control-Allow-Methods", "*")            //允许访问所有域
-
+	w.Header().Set("Access-Control-Allow-Origin", "*")  //允许访问所有域
+	w.Header().Set("Access-Control-Allow-Methods", "*") //允许访问所有域
+	
 	err := r.ParseForm() //解析表单                 //获取文件内容
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	imgFile, _, err := r.FormFile("f") //获取文件内容
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	defer imgFile.Close()
 	imgName := ""
@@ -68,24 +75,30 @@ func Upload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 	p = strings.Replace(p, "../", "/", -1)
 	p = path.Join(config.BASEPATH, p)
-	uuid := uuid.New()
-	fname := fmt.Sprintf("%x%x%x%x%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:16])
+	_uuid := uuid.New()
+	fname := fmt.Sprintf("%x%x%x%x%x", _uuid[0:4], _uuid[4:6], _uuid[6:8], _uuid[8:10], _uuid[10:16])
 	imgPath := path.Join(p, fname+path.Ext(imgName))
-
+	
 	saveFile, err := os.Create(imgPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
+	if saveFile != nil {
+		defer saveFile.Close()
+	}
+	
 	s, err := io.Copy(saveFile, imgFile) //保存
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-
+	
 	if s == 0 {
-		log.Fatal("保存不成功")
+		log.Println("保存不成功")
+		return
 	}
-	defer saveFile.Close()
-
+	
 	resData := model.JsonResult{Code: 200, Msg: ""}
 	msg, _ := json.Marshal(resData)
 	w.Write(msg)
@@ -98,10 +111,10 @@ func Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 	p = strings.Replace(p, "../", "/", -1)
 	p = path.Join(config.BASEPATH, p)
-
+	
 	err := os.Remove(p)
 	fmt.Println(err)
-
+	
 	resData := model.JsonResult{Code: 200, Msg: ""}
 	msg, _ := json.Marshal(resData)
 	w.Write(msg)
